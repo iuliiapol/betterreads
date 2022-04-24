aecad5b6cf3f83f148d95363c47ed2bcf113cdd0
austin-snyder <66277645+austin-snyder@users.noreply.github.com>
2022-04-24 13:12:40 -0400

    consolidate search feature into one function

--- apitest.py
+++ apitest.py
@@ -1,76 +1,43 @@
-# using a JSON object bc that seems easiest
 import json
-
-# same reason as JSON---seems easiest way to access API
 from urllib.request import urlopen
 
-# just for testing in terminal
+# terminal testing
 searchPhrase = input("Search for a book...: ").strip()
-api = "https://www.googleapis.com/books/v1/volumes?q="
-books = []
 
-# searching google books by title
-def search_by_title(userResponse):
-    resp = urlopen(api + 'title:' + userResponse)
-    book_data = json.load(resp)
-    for book in book_data['items']:
-        info = book['volumeInfo']
-        books.append({'Title': info.get('title', ''),
-                      'Authors': info.get('authors', ''),
-                      'Publisher': info.get('publisher', ''),
-                      'Description': info.get('description', ''),
-                      'Categories': info.get('categories', ''),
-                      'Rating': info.get('averageRating', 0),
-                      'Ratings Count': info.get('ratingsCount', 0),
-                      'Page Count': info.get('pageCount', 0),
-                      'Publishing Date': info.get('publishedDate', 0),
-                      'Cover Thumbnail': info['imageLinks']['thumbnail'],
-                      'ISBN': info['industryIdentifiers'][0]['identifier']})
+def search(searchPhrase):
+    searchKeys = ['title', 'authors', 'publisher', 'description', 'categories', 'publishedDate']
+    api = "https://www.googleapis.com/books/v1/volumes?q="
+    books = []
 
-# searching google books by author
-def search_by_author(userResponse):
-    resp = urlopen(api + 'authors:' + userResponse)
-    book_data = json.load(resp)
-    for book in book_data['items']:
-        info = book['volumeInfo']
-        books.append({'Title': info.get('title', ''),
-                      'Authors': info.get('authors', ''),
-                      'Publisher': info.get('publisher', ''),
-                      'Description': info.get('description', ''),
-                      'Categories': info.get('categories', ''),
-                      'Rating': info.get('averageRating', 0),
-                      'Ratings Count': info.get('ratingsCount', 0),
-                      'Page Count': info.get('pageCount', 0),
-                      'Publishing Date': info.get('publishedDate', 0),
-                      'Cover Thumbnail': info['imageLinks']['thumbnail'],
-                      'ISBN': info['industryIdentifiers'][0]['identifier']})
+    for searchKey in searchKeys:
+        resp = urlopen(api + searchKey + ':' + searchPhrase)
+        book_data = json.load(resp)
+        for book in book_data['items']:
+            info = book['volumeInfo']
+            helpfulInfo = {'Title': info.get('title', ''),
+                        'Authors': info.get('authors', ''),
+                        'Publisher': info.get('publisher', ''),
+                        'Description': info.get('description', ''),
+                        'Categories': info.get('categories', ''),
+                        'Rating': info.get('averageRating', 0),
+                        'Ratings Count': info.get('ratingsCount', 0),
+                        'Page Count': info.get('pageCount', 0),
+                        'Publishing Date': info.get('publishedDate', 0)}
+            if 'imageLinks' in info:
+                helpfulInfo['Cover Thumbnail'] = info.get('imageLinks')['thumbnail'].replace('1', '10')
+            else:
+                helpfulInfo['Cover Thumbnail'] = ''
+            
+            if 'industryIdentifiers' in info:
+                helpfulInfo['ISBN'] = info.get('industryIdentifiers')[0]['identifier']
+            else:
+                helpfulInfo['ISBN'] = ''
+            books.append(helpfulInfo)
+    
+    uniqueBooks = list({ each['ISBN'] : each for each in books }.values())
+    return uniqueBooks
 
-# searching google books by publishing date
-def search_by_publishingDate(userResponse):
-    resp = urlopen(api + 'publishedDate:' + userResponse)
-    book_data = json.load(resp)
-    for book in book_data['items']:
-        info = book['volumeInfo']
-        books.append({'Title': info.get('title', ''),
-                      'Authors': info.get('authors', ''),
-                      'Publisher': info.get('publisher', ''),
-                      'Description': info.get('description', ''),
-                      'Categories': info.get('categories', ''),
-                      'Rating': info.get('averageRating', 0),
-                      'Ratings Count': info.get('ratingsCount', 0),
-                      'Page Count': info.get('pageCount', 0),
-                      'Publishing Date': info.get('publishedDate', 0),
-                      'Cover Thumbnail': info['imageLinks']['thumbnail'],
-                      'ISBN': info['industryIdentifiers'][0]['identifier']})
+# terminal testing
+searchResponse = search(searchPhrase)
 
-# remove duplicate books
-def clean_book_list(list):
-    unique = { each['Title'] : each for each in list }.values()
-    return unique
-
-# just run the functions for testing in terminal
-search_by_title(searchPhrase)
-search_by_author(searchPhrase)
-search_by_publishingDate(searchPhrase)
-cleaned_book_list = clean_book_list(books)
-
+# print(searchResponse)
