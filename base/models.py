9f77a499cf297aa4e95f41c46b06fba8611c3f30
iuliiapol <iuliia@live.unc.edu>
2022-04-25 15:43:05 -0400

    Fixed bug in the import statement for models.py

--- base/models.py
+++ base/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from platformdirs import user_log_dir
 
 # Create your models here.
 
